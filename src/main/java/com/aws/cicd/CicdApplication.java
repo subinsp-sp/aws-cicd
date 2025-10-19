73ef2c991be5ae982b516216590ba3e075277cb0
Subin <subinsudhakar19922gmail.com>
2025-10-19 12:24:37 +0530

    first commit

---
+++ src/main/java/com/aws/cicd/CicdApplication.java
@@ -0,0 +1,21 @@
+package com.aws.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+	@GetMapping("/first")
+	public String firstDeployment(){
+		return "This is my first commit to GitHub";
+	}
+
+}
